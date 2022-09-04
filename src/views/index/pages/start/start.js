eac3d3cf2087012fa8c2ee00756456148e005669
liuguo <liuguogy@gmail.com>
2022-09-04 20:17:23 +0800

    优化页面显示

--- src/views/index/pages/start/start.js
+++ src/views/index/pages/start/start.js
@@ -13,8 +13,19 @@ const dbPath = 'photos.db'
 
 const db = Sqlite.getInstance();
 
-class ScanContent extends React.Component {
+class ScanLoading extends React.Component {
+    render() {
+        return (<div className={styles.spinner_parent}>
+            <div className={styles.spinner}>
+                <div></div>
+                <div></div>
+                <p>{this.props.text}</p>
+            </div>
+        </div>);
+    }
+}
 
+class ScanContent extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -26,42 +37,68 @@ class ScanContent extends React.Component {
             fileNum: 0,
             //已扫描的数量
             scanNum: 0,
+            //状态: 0未扫描，1扫描中，2删除中
+            status: 0,
         }
     }
 
     async handleClick() {
-        let result = await remote.dialog.showOpenDialog({
-            title: "选择目录",
-            properties: ['openDirectory'],
-        });
-        await this.checkDB();
-        if (!result.canceled && result.filePaths) {
-            let dirPath = result.filePaths[0];
-            console.log(dirPath);
+        if (this.state.status === 0) {
+            let result = await remote.dialog.showOpenDialog({
+                title: "选择目录",
+                properties: ['openDirectory'],
+            });
             this.setState({
                 fileNum: 0,
                 scanNum: 0,
+                progress: 0,
             })
-            await this.findAllfiles(dirPath, async () => {
+            await this.checkDB();
+            if (!result.canceled && result.filePaths) {
+                let dirPath = result.filePaths[0];
+                console.log(dirPath);
                 this.setState({
-                    fileNum: this.state.fileNum + 1,
+                    fileNum: 0,
+                    scanNum: 0,
+                    status: 1,
+                    progress: 0,
+                })
+                await this.findAllfiles(dirPath, async () => {
+                    if (this.state.status === 1) {
+                        this.setState({
+                            fileNum: this.state.fileNum + 1,
+                            toast: "已发现 " + (this.state.fileNum + 1) + " 个照片",
+                        });
+                    }
                 });
-            });
-            console.log(this.state.fileNum);
-            await db.connect(Path.join(userPath, dbPath));
-            await this.findAllfiles(dirPath, async (file) => {
-                await this.handleFile(file);
-            });
-            await db.connect(Path.join(userPath, dbPath));
-            let res = await db.all(`SELECT * FROM photos
-            WHERE id > (
-              SELECT MIN(id) FROM photos d2  
-              WHERE photos.hash = d2.hash
-            )`);
-            console.log("结果：")
-            console.log(res);
-            db.close();
+                console.log(this.state.fileNum);
+                await db.connect(Path.join(userPath, dbPath));
+                await this.findAllfiles(dirPath, async (file) => {
+                    await this.handleFile(file);
+                });
+                await db.connect(Path.join(userPath, dbPath));
+                let res = await db.all(`SELECT * FROM photos
+                WHERE id > (
+                  SELECT MIN(id) FROM photos d2  
+                  WHERE photos.hash = d2.hash
+                )`);
+                console.log("结果：")
+                console.log(res);
+                db.close();
+                this.setState({
+                    status: 0,
+                })
+            }
+        } else if (this.state.status === 1) {
+            this.setState({
+                status: 0,
+                fileNum: 0,
+                scanNum: 0,
+                toast: "",
+                progress: 0,
+            })
         }
+
     }
 
     async checkDB() {
@@ -78,9 +115,7 @@ class ScanContent extends React.Component {
     async handleFile(filePath) {
         this.setState({
             toast: "正在扫描：" + filePath,
-        })
-        // let fileData = await this.readFile(filePath);
-        // let hash = MD5(fileData).toString().toUpperCase();
+        });
         let hash = await this.getMD5(filePath);
         let fileName = Path.basename(filePath);
         await db.run('INSERT INTO photos (filename, path, hash) VALUES(?, ?, ?)', [fileName, filePath, hash]);
@@ -112,11 +147,38 @@ class ScanContent extends React.Component {
         });
     }
 
+    readdir(path) {
+        return new Promise(function (resolve, reject) {
+            Fs.readdir(path, (err, files) => {
+                if (err)
+                    reject(err);
+                else
+                    resolve(files);
+            });
+        });
+    }
+
+    lstat(path) {
+        return new Promise(function (resolve, reject) {
+            Fs.lstat(path, (err, stats) => {
+                if (err)
+                    reject(err);
+                else {
+                    resolve(stats);
+                }
+            });
+        });
+
+    }
+
     async findAllfiles(fileRootPath, cb) {
-        let files = Fs.readdirSync(fileRootPath);
+        let files = await this.readdir(fileRootPath);
         for (let i = 0; i < files.length; i++) {
+            if (this.state.status === 0) {
+                return;
+            }
             let filePath = Path.join(fileRootPath, files[i]);
-            let stat = Fs.lstatSync(filePath);
+            let stat = await this.lstat(filePath);
             if (stat.isDirectory()) {
                 //继续寻找子目录
                 await this.findAllfiles(filePath, cb);
@@ -136,19 +198,15 @@ class ScanContent extends React.Component {
                 {/* <div className={styles.dir_parent}>
                     <button className={styles.sel_dir_button} onClick={() => { this.handleClick() }}>选择目录</button>
                 </div> */}
-                <div className={styles.spinner_parent}>
-                    <div className={styles.spinner}>
-                        <div></div>
-                        <div></div>
-                        <p>{this.state.progress + "%"}</p>
-                    </div>
-                </div>
+                {(this.state.status === 0) ?
+                        null :
+                        <ScanLoading text={this.state.progress + "%"}> </ScanLoading>}
                 <div className={styles.toast_parent}>
                     <p >{this.state.toast}</p>
                 </div>
                 <div className={styles.scan_or_cancel_button_parent}>
                     <div className={styles.scan_or_cancel_button}>
-                        <button className={styles.buttonStress} onClick={() => { this.handleClick() }}>开始扫描</button>
+                        <button className={(this.state.status === 0) ? styles.buttonStress : styles.buttonWarn} onClick={() => { this.handleClick() }}>{(this.state.status === 0) ? "开始扫描" : "结束扫描"}</button>
                     </div>
                 </div>
             </div>
@@ -163,12 +221,6 @@ class PageStart extends React.Component {
 
         if (e.target.files) {
             let file = e.target.files[0];
-            let reader = new FileReader();
-            reader.onloadend = function () {
-                var hash = MD5(reader.result);
-                console.log(hash.toString().toUpperCase());
-            }
-            reader.readAsArrayBuffer(file);
             EXIF.getData(file, function () {
                 let exifData = EXIF.pretty(this);
                 if (exifData) {
@@ -180,8 +232,6 @@ class PageStart extends React.Component {
             });
         }
     }
-
-
 
     render() {
         console.log("用户数据目录：" + userPath);
